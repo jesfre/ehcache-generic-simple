ae02cb7806aba8642d505a6e53ff6958f682dd80
Jorge <Jorge>
2016-04-11 02:15:12 -0400

    Initial commit. Main functionality added.

---
+++ src/main/java/com/blogspot/jesfre/cache/simplegeneric/GenericSimpleCache.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package com.blogspot.jesfre.cache.simplegeneric;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.config.CacheConfiguration;
+
+/**
+ * A simplistic reduced delegation from {@link Cache}
+ * @author <a href="jorge.ruiz.aquino@gmail.com">Jorge Ruiz Aquino</a>
+ * Apr 9, 2016
+ * @param <K>
+ * @param <V>
+ */
+public class GenericSimpleCache<K,V> implements IGenericCache<K, V> {
+	private String cacheName = null;
+	private CacheManager cacheManager = null;
+	
+	/**
+	 * Gets the default configuration.
+	 * @return
+	 */
+	public static CacheConfiguration getDefaultConfiguration() {
+		return CacheManager.getInstance().getConfiguration().getDefaultCacheConfiguration();
+	}
+	
+	/**
+	 * Creates and registers the new cache.
+	 * @param cacheName
+	 * @param cacheManagerInstance
+	 */
+	public GenericSimpleCache(String cacheName, CacheManager cacheManagerInstance) {
+		this.cacheName = cacheName;
+		this.cacheManager = cacheManagerInstance;
+		this.register();
+	}
+	
+	/**
+	 * Registers this cache to the cache manager using the default configuration. 
+	 */
+	public void register() {
+		cacheManager.addCache(cacheName);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void put(K key, V value) {
+		if(getCache() == null) {
+			// In case someone had removed the cache
+			register();
+		}
+		Element element = new Element(key, value);
+		getCache().put(element);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public V get(K key) {
+		Element element = this.getCache().get(key);
+		if(element != null) {
+			return (V)element.getObjectValue();
+		}
+		return null;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean containsKey(K key) {
+		return getCache().isKeyInCache(key);
+	}
+	
+	/**
+	 * Returns the cache wrapped by this instance.
+	 * @return
+	 */
+	public Cache getCache() {
+		return this.cacheManager.getCache(cacheName);
+	}
+
+	/**
+	 * Removes all elements in the cache.
+	 */
+	public void removeAll() {
+		getCache().removeAll();
+	}
+}
---
+++ src/main/java/com/blogspot/jesfre/cache/simplegeneric/IGenericCache.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.blogspot.jesfre.cache.simplegeneric;
+
+/**
+ * A generic interface for generic enabled cache wrappers. 
+ * @author <a href="jorge.ruiz.aquino@gmail.com">Jorge Ruiz Aquino</a>
+ * Apr 9, 2016
+ * @param <K>
+ * @param <V>
+ */
+public interface IGenericCache<K, V> {
+
+	/**
+	 * Puts the new value into the cache.
+	 * @param key
+	 * @param value
+	 */
+	void put(K key, V value);
+	
+	/**
+	 * Returns the value for the given key.
+	 * @param key
+	 * @return
+	 */
+	V get(K key);
+	
+	/**
+	 * Checks if the given key exists in the cache.
+	 * @param key
+	 * @return
+	 */
+	boolean containsKey(K key);
+	
+}
